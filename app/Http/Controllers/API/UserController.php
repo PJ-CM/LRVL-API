182f06dfd81860fe12132cbdf65baa5b3db1e2ff
PJCM-ZubiMan <pcrespoma18dw@ikzubirimanteo.com>
2019-01-23 13:58:50 +0100

    Práctica de uso de una API

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $valores = User::orderBy('id', 'desc')->get();
+        return $valores;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dato = new User;
+        $dato->name = $request->name;
+        $dato->username = $request->username;
+        $dato->direction = $request->direction;
+        $dato->phone = $request->phone;
+        $dato->country = $request->country;
+        $dato->email = $request->email;
+        $dato->password = Hash::make($request->password);
+        $dato->activo = $request->activo;
+        $dato->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
